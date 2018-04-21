afcef21de6f4954f76e9c9e22fb0b14007fc90e8
Matija Marohnić <matija.marohnic@gmail.com>
2018-04-21 19:34:35 +0200

    Fix layout for the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,18 @@
 // @flow
 import * as React from 'react'
+import Container from '../components/container'
 import Text from '../components/text'
 
 const NotFoundPage = () => (
-  <Text>
-    <h1>Page Not Found</h1>
-    <p>
-      For whatever reason this page doesn't exist anymore, but it's likely that
-      the content still exists elsewhere on the site.
-    </p>
-  </Text>
+  <Container>
+    <Text>
+      <h1>Page Not Found</h1>
+      <p>
+        This page doesn't exist anymore, but it's likely that the content you're
+        looking for still exists elsewhere on the site.
+      </p>
+    </Text>
+  </Container>
 )
 
 export default NotFoundPage
