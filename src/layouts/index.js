c1f5f2c88c11d2b263f94c6437b54bccdc3e0849
Matija Marohnić <matija.marohnic@gmail.com>
2018-06-03 17:28:57 +0200

    Remove extraneous charset meta tag
    Gatsby already adds this.

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -56,7 +56,6 @@ const DefaultTemplate = ({ data, children }: Props) => {
         <Helmet>
           <html lang="en" />
           <title>{title}</title>
-          <meta charSet="utf-8" />
           <meta name="description" content={tagline} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta
