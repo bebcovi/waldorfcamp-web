5a1689c5720d0710961ed3273d563e0741f992f7
Matija Marohnić <matija.marohnic@gmail.com>
2018-06-27 22:11:05 +0200

    Add tracking code only in production

--- src/components/tracking-code.js
+++ src/components/tracking-code.js
@@ -1,25 +1,28 @@
 import React from 'react'
 
-const TrackingCode = () => (
-  <script
-    // eslint-disable-next-line react/no-danger
-    dangerouslySetInnerHTML={{
-      __html: `
-        var _gauges = _gauges || [];
-        (function() {
-          var t   = document.createElement('script');
-          t.type  = 'text/javascript';
-          t.async = true;
-          t.id    = 'gauges-tracker';
-          t.setAttribute('data-site-id', '519e46ecf5a1f51adc000073');
-          t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-          t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(t, s);
-        })();
-      `,
-    }}
-  />
-)
+const code = `
+  var _gauges = _gauges || [];
+  (function() {
+    var t   = document.createElement('script');
+    t.type  = 'text/javascript';
+    t.async = true;
+    t.id    = 'gauges-tracker';
+    t.setAttribute('data-site-id', '519e46ecf5a1f51adc000073');
+    t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+    t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(t, s);
+  })();
+`
+
+const TrackingCode = () =>
+  process.env.NODE_ENV === 'production' ? (
+    <script
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{
+        __html: code,
+      }}
+    />
+  ) : null
 
 export { TrackingCode }
