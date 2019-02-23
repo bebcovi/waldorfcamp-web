d1d02e96d670ac8f180f2cc1958382013701e711
Matija Marohnić <matija.marohnic@gmail.com>
2019-02-23 03:16:25 +0100

    Use Gatsby Babel preset

--- test/transformer.js
+++ test/transformer.js
@@ -1,24 +1,8 @@
 module.exports = require('babel-jest').createTransformer({
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-flow',
-  ],
+  presets: ['gatsby', '@babel/flow'],
   plugins: [
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-syntax-dynamic-import',
-    'babel-plugin-lodash',
-    'babel-plugin-polished',
-    'babel-plugin-date-fns',
     [
-      'babel-plugin-emotion',
+      'emotion',
       {
         hoist: true,
         autoLabel: true,
