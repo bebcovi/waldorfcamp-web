179b3b962df7839e8c48520baae3c8c675d32220
Matija Marohnić <matija.marohnic@gmail.com>
2019-08-15 16:32:02 +0200

    Round currency conversion to whole numbers

--- src/utils/currency.js
+++ src/utils/currency.js
@@ -3,4 +3,4 @@
 export const EUR_TO_HRK = 7.44511
 
 export const fromHrkToEur = (amountHrk: number): number =>
-  Math.round((amountHrk / EUR_TO_HRK) * 10) / 10
+  Math.round(amountHrk / EUR_TO_HRK)
