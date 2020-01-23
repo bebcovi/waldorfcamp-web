955678e0b354fad359762eb9844a525f30dc113e
Matija Marohnić <matija.marohnic@gmail.com>
2020-01-23 19:41:43 +0100

    Don't add trailing slashes during development

--- next.config.js
+++ next.config.js
@@ -2,20 +2,24 @@ const withPlugins = require('next-compose-plugins')
 const mdx = require('@zeit/next-mdx')
 const smartypants = require('@silvenon/remark-smartypants')
 const env = require('dotenv-safe').config()
+const { PHASE_EXPORT } = require('next/constants')
 
-module.exports = withPlugins(
-  [
-    mdx({
-      options: {
-        remarkPlugins: [smartypants],
+module.exports = (...args) => {
+  const [phase] = args
+  return withPlugins(
+    [
+      mdx({
+        options: {
+          remarkPlugins: [smartypants],
+        },
+      }),
+      {
+        pageExtensions: ['js', 'jsx', 'md', 'mdx'],
       },
-    }),
+    ],
     {
-      pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+      env,
+      exportTrailingSlash: phase === PHASE_EXPORT,
     },
-  ],
-  {
-    env,
-    exportTrailingSlash: true,
-  },
-)
+  )(...args)
+}
