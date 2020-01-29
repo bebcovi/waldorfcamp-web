155898d7e8e2961e98afc62ce786a85711c59c23
Matija Marohnić <matija.marohnic@gmail.com>
2020-01-29 01:35:58 +0100

    Print out a success message on images download

--- scripts/fetch-gallery-images.js
+++ scripts/fetch-gallery-images.js
@@ -25,6 +25,7 @@ const fetchGalleryImages = async () => {
       aspectRatio: r.aspect_ratio,
     }))
     await fs.outputJson(FILE, images, { spaces: 2 })
+    console.log(`\n🖼  ${chalk.green('Images downloaded!')}`)
   } catch (err) {
     console.error(chalk.red(err))
   }
