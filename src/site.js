898e421c4f7a8bd526f39682eeb2e8e2102cdf4a
Alen Lagundzija <alen@annovation.hr>
2021-04-28 18:01:52 +0200

    Registration deadline removed

--- src/site.js
+++ src/site.js
@@ -5,7 +5,7 @@ export default {
   title: 'International Waldorf Camp Croatia',
   tagline: 'An active and dynamic vacation',
   registrations: {
-    accepting: false,
+    accepting: true,
     deadline: '2021-04-01',
   },
   startDate: '2021-08-01',
